65250dc395ee6580ba5ccb773a5b573b68addb42
twl <tuwanliff@163.com>
2018-12-06 20:13:31 +0800

    初始版本

---
+++ app/src/main/java/com/boredream/videoplayer/MockUtils.java
@@ -0,0 +1,97 @@
+package com.boredream.videoplayer;
+
+import android.content.Context;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MockUtils {
+
+    public static <T> T mockData(Class<T> clazz, String path) {
+        T t = null;
+        try {
+            t = clazz.newInstance();
+            setValue(t, clazz, path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+
+//    public static <T> List<T> mockList(Class<T> clazz) {
+//        return mockList(clazz, 10);
+//    }
+//
+//    public static <T> List<T> mockList(Class<T> clazz, int count) {
+//        List<T> list = new ArrayList<>();
+//        for (int i = 0; i < count; i++) {
+//            T t = mockData(clazz);
+//            if (t != null) {
+//                list.add(t);
+//            }
+//        }
+//        return list;
+//    }
+
+    private static <T> void setValue(Object object, Class<T> clazz, String path) throws IllegalAccessException {
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            if (Modifier.isFinal(field.getModifiers()) || Modifier.isStatic(field.getModifiers())) {
+                continue;
+            }
+
+            Object value = getFieldMockValue(field, path);
+            if (value != null) {
+                field.setAccessible(true);
+                field.set(object, value);
+            }
+        }
+    }
+
+    /**
+     * 模拟基础类型数据
+     */
+    private static Object getFieldMockValue(Field field, String path) {
+        Class clazzType = field.getType();
+        Object value = null;
+        if (clazzType == int.class || clazzType == Integer.class) {
+            value = new Random().nextInt(100);
+        }
+        else if (clazzType == long.class || clazzType == Long.class) {
+            value = Math.abs(new Random().nextLong() % 10000);
+        }
+        else if (clazzType == float.class || clazzType == Float.class) {
+            value = new Random().nextFloat();
+        }
+        else if (clazzType == double.class || clazzType == Double.class) {
+            value = new Random().nextDouble();
+        }
+        else if (clazzType == String.class) {
+//            String name = field.getName();
+//            if (name.toLowerCase().contains("img") || name.toLowerCase().contains("url")) {
+//                value = getImgUrl();
+//            }
+//            else if (name.toLowerCase().contains("video")) {
+//                value = getVideoUrl();
+//            }
+//            else {
+//                value = name + "_" + new Random().nextInt(100);
+//            }
+            value = path;
+        }
+        else if (clazzType == boolean.class || clazzType == Boolean.class) {
+            value = new Random().nextBoolean();
+        }
+        return value;
+    }
+
+}
