cbdf9c1a58eb7636b371c135b23816bd28480401
twl <tuwanliff@163.com>
2018-12-08 17:48:14 +0800

    动态获取权限搞定，可播放版本

--- app/src/main/java/com/boredream/videoplayer/MainActivity.java
+++ app/src/main/java/com/boredream/videoplayer/MainActivity.java
@@ -1,13 +1,21 @@
 package com.boredream.videoplayer;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -22,10 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        verifyStoragePermissions(this);
         findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                VideoDetailInfo info = MockUtils.mockData(VideoDetailInfo.class, null);
+                VideoDetailInfo info = MockUtils.mockData(VideoDetailInfo.class, "/mnt/sdcard/Movies/15825839-1-16.mp4");
                 VideoDetailActivity.start(MainActivity.this, info);
             }
         });
@@ -41,20 +50,135 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE" };
+
+    /**
+     * 动态获取权限
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+            try {
+                //检测是否有写的权限
+                int permission = ActivityCompat.checkSelfPermission(activity,
+                "android.permission.WRITE_EXTERNAL_STORAGE");
+                if (permission != PackageManager.PERMISSION_GRANTED) {
+                    // 没有写的权限，去申请写的权限，会弹出对话框
+                    ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+             }
+        }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {//是否选择，没选择就不会继续
             Uri uri = data.getData();//得到uri
-            String[] proj = {MediaStore.Images.Media.DATA};
-            Cursor actualimagecursor = getContentResolver().query(uri, proj, null, null, null);
-            int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            actualimagecursor.moveToFirst();
-            String img_path = actualimagecursor.getString(actual_image_column_index);
-            //img_path = getPath(this, uri).toString() || uri.getPath().toString()
-            actualimagecursor.close();
+            String[] proj = {MediaStore.Video.Media.DATA};
+            String img_path = null;
+//            Cursor cursor = null;
+//            try
+//            {
+//                cursor = managedQuery(uri, proj, null, null, null);
+//                int column_index = cursor.getColumnIndex(MediaStore.Video.Media.DATA);
+//                cursor.moveToFirst();
+//                img_path = cursor.getString(column_index);
+//    //            img_path = getPath(this, uri).toString() || uri.getPath().toString()
+//            }
+//            finally {
+//                if (cursor != null)
+//                    cursor.close();
+//            }
+            img_path = getPathByUri4kitkat(MainActivity.this, uri);
             VideoDetailInfo info = MockUtils.mockData(VideoDetailInfo.class, img_path);
             VideoDetailActivity.start(MainActivity.this, info);
         }
+    }
+
+    @SuppressLint("NewApi")
+    public static String getPathByUri4kitkat(final Context context, final Uri uri) {
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {// ExternalStorageProvider
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            } else if (isDownloadsDocument(uri)) {// DownloadsProvider
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),
+                        Long.valueOf(id));
+                return getDataColumn(context, contentUri, null, null);
+            } else if (isMediaDocument(uri)) {// MediaProvider
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] { split[1] };
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {// MediaStore
+            // (and
+            // general)
+            return getDataColumn(context, uri, null, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {// File
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = { column };
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    /**
+     * @param uri
+     *            外部存储
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri
+     *            下载
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri
+     *            媒体
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 }
